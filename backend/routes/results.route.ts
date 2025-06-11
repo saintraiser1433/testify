ce29145bc517771235659c49d47d62d5c354ca96
John Rey Decosta <decosta.johnrey30@gmail.com>
2025-06-11 15:20:39 +0800

    remove authenticate token in admin

--- backend/routes/results.route.ts
+++ backend/routes/results.route.ts
@@ -8,8 +8,8 @@ import { authenticateToken } from '../middlewares/auth.middleware';
 const route = Router();
 // authenticateToken
 
-route.get('/rank/:examineeId', authenticateToken, fetchExamineeRank);
-route.get('/:examineeId', authenticateToken, getSummaryByExaminee);
-route.get('/', authenticateToken, getAllResult);
+route.get('/rank/:examineeId', fetchExamineeRank);
+route.get('/:examineeId', getSummaryByExaminee);
+route.get('/', getAllResult);
 
 export default route;
